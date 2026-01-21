247c50ab00f9ac5213a422c4c5f322743b4b22ce
MASINA Elliot <elliot.masina@epitech.eu>
2026-01-21 10:50:45 +0100

    Fix: X11 and glfw missing key in keyboard mapping

--- plugins/Window/X11/src/x11.cpp
+++ plugins/Window/X11/src/x11.cpp
@@ -3,44 +3,179 @@
 #include "Utils/Image.hpp"
 #include "Utils/Logger.hpp"
 
-#include <X11/Xlib.h>
 #include <X11/keysym.h>
 
+#include <X11/XKBlib.h>
+#include <utility>
 #include <vector>
 
-static cae::KeyCode translateKeycode(const unsigned int keycode)
+static cae::KeyCode translateKeysym(const KeySym sym)
 {
-    switch (keycode)
+    switch (sym)
     {
-        case 25:
-            return cae::KeyCode::W;
-        case 38:
+        case XK_a:
             return cae::KeyCode::A;
-        case 39:
-            return cae::KeyCode::S;
-        case 40:
+        case XK_b:
+            return cae::KeyCode::B;
+        case XK_c:
+            return cae::KeyCode::C;
+        case XK_d:
             return cae::KeyCode::D;
+        case XK_e:
+            return cae::KeyCode::E;
+        case XK_f:
+            return cae::KeyCode::F;
+        case XK_g:
+            return cae::KeyCode::G;
+        case XK_h:
+            return cae::KeyCode::H;
+        case XK_i:
+            return cae::KeyCode::I;
+        case XK_j:
+            return cae::KeyCode::J;
+        case XK_k:
+            return cae::KeyCode::K;
+        case XK_l:
+            return cae::KeyCode::L;
+        case XK_m:
+            return cae::KeyCode::M;
+        case XK_n:
+            return cae::KeyCode::N;
+        case XK_o:
+            return cae::KeyCode::O;
+        case XK_p:
+            return cae::KeyCode::P;
+        case XK_q:
+            return cae::KeyCode::Q;
+        case XK_r:
+            return cae::KeyCode::R;
+        case XK_s:
+            return cae::KeyCode::S;
+        case XK_t:
+            return cae::KeyCode::T;
+        case XK_u:
+            return cae::KeyCode::U;
+        case XK_v:
+            return cae::KeyCode::V;
+        case XK_w:
+            return cae::KeyCode::W;
+        case XK_x:
+            return cae::KeyCode::X;
+        case XK_y:
+            return cae::KeyCode::Y;
+        case XK_z:
+            return cae::KeyCode::Z;
 
-        case 111:
-            return cae::KeyCode::Up;
-        case 116:
-            return cae::KeyCode::Down;
-        case 113:
-            return cae::KeyCode::Left;
-        case 114:
-            return cae::KeyCode::Right;
+        // Numbers
+        case XK_0:
+            return cae::KeyCode::Num0;
+        case XK_1:
+            return cae::KeyCode::Num1;
+        case XK_2:
+            return cae::KeyCode::Num2;
+        case XK_3:
+            return cae::KeyCode::Num3;
+        case XK_4:
+            return cae::KeyCode::Num4;
+        case XK_5:
+            return cae::KeyCode::Num5;
+        case XK_6:
+            return cae::KeyCode::Num6;
+        case XK_7:
+            return cae::KeyCode::Num7;
+        case XK_8:
+            return cae::KeyCode::Num8;
+        case XK_9:
+            return cae::KeyCode::Num9;
 
-        case 65:
-            return cae::KeyCode::Space;
-        case 37:
-            return cae::KeyCode::LCtrl;
-        case 50:
+        // Modifiers
+        case XK_Shift_L:
             return cae::KeyCode::LShift;
-        case 64:
+        case XK_Shift_R:
+            return cae::KeyCode::RShift;
+        case XK_Control_L:
+            return cae::KeyCode::LCtrl;
+        case XK_Control_R:
+            return cae::KeyCode::RCtrl;
+        case XK_Alt_L:
             return cae::KeyCode::LAlt;
+        case XK_Alt_R:
+            return cae::KeyCode::RAlt;
+        case XK_Super_L:
+            return cae::KeyCode::LSuper;
+        case XK_Super_R:
+            return cae::KeyCode::RSuper;
+        case XK_Caps_Lock:
+            return cae::KeyCode::CapsLock;
 
-        case 9:
+        // Navigation
+        case XK_Up:
+            return cae::KeyCode::Up;
+        case XK_Down:
+            return cae::KeyCode::Down;
+        case XK_Left:
+            return cae::KeyCode::Left;
+        case XK_Right:
+            return cae::KeyCode::Right;
+        case XK_Home:
+            return cae::KeyCode::Home;
+        case XK_End:
+            return cae::KeyCode::End;
+        case XK_Page_Up:
+            return cae::KeyCode::PageUp;
+        case XK_Page_Down:
+            return cae::KeyCode::PageDown;
+
+        // Editing
+        case XK_Return:
+            return cae::KeyCode::Enter;
+        case XK_BackSpace:
+            return cae::KeyCode::Backspace;
+        case XK_Tab:
+            return cae::KeyCode::Tab;
+        case XK_space:
+            return cae::KeyCode::Space;
+        case XK_Delete:
+            return cae::KeyCode::Delete;
+        case XK_Insert:
+            return cae::KeyCode::Insert;
+
+        // Function keys
+        case XK_F1:
+            return cae::KeyCode::F1;
+        case XK_F2:
+            return cae::KeyCode::F2;
+        case XK_F3:
+            return cae::KeyCode::F3;
+        case XK_F4:
+            return cae::KeyCode::F4;
+        case XK_F5:
+            return cae::KeyCode::F5;
+        case XK_F6:
+            return cae::KeyCode::F6;
+        case XK_F7:
+            return cae::KeyCode::F7;
+        case XK_F8:
+            return cae::KeyCode::F8;
+        case XK_F9:
+            return cae::KeyCode::F9;
+        case XK_F10:
+            return cae::KeyCode::F10;
+        case XK_F11:
+            return cae::KeyCode::F11;
+        case XK_F12:
+            return cae::KeyCode::F12;
+
+        // System
+        case XK_Escape:
             return cae::KeyCode::Escape;
+        case XK_Print:
+            return cae::KeyCode::PrintScreen;
+        case XK_Pause:
+            return cae::KeyCode::Pause;
+        case XK_Menu:
+            return cae::KeyCode::Menu;
+
         default:
             return cae::KeyCode::Count;
     }
@@ -126,12 +261,12 @@ void cae::X11::setIcon(const std::string &path) const
         iconData.push_back(static_cast<unsigned long>(image.height));
 
         const uint8_t *pixels = image.pixels;
-        for (size_t i = 0; i < pixelCount; ++i)
+        for (size_t i = 0; std::cmp_less(i, pixelCount); ++i)
         {
-            const uint8_t r = pixels[i * 4 + 0];
-            const uint8_t g = pixels[i * 4 + 1];
-            const uint8_t b = pixels[i * 4 + 2];
-            const uint8_t a = pixels[i * 4 + 3];
+            const uint8_t r = pixels[(i * 4) + 0];
+            const uint8_t g = pixels[(i * 4) + 1];
+            const uint8_t b = pixels[(i * 4) + 2];
+            const uint8_t a = pixels[(i * 4) + 3];
 
             const unsigned long argb = (static_cast<unsigned long>(a) << 24) | (static_cast<unsigned long>(r) << 16) |
                                        (static_cast<unsigned long>(g) << 8) | (static_cast<unsigned long>(b));
@@ -173,16 +308,26 @@ bool cae::X11::pollEvent(WindowEvent &outEvent)
         switch (event.type)
         {
             case KeyPress:
+            {
                 e.type = WindowEventType::KeyDown;
-                e.key.key = translateKeycode(event.xkey.keycode);
+
+                const KeySym sym = XkbKeycodeToKeysym(m_display, event.xkey.keycode, 0, 0);
+
+                e.key.key = translateKeysym(sym);
                 m_eventQueue.push(e);
                 break;
+            }
 
             case KeyRelease:
+            {
                 e.type = WindowEventType::KeyUp;
-                e.key.key = translateKeycode(event.xkey.keycode);
+
+                const KeySym sym = XkbKeycodeToKeysym(m_display, event.xkey.keycode, 0, 0);
+
+                e.key.key = translateKeysym(sym);
                 m_eventQueue.push(e);
                 break;
+            }
 
             case ConfigureNotify:
                 m_frameBufferResized = true;
@@ -195,7 +340,7 @@ bool cae::X11::pollEvent(WindowEvent &outEvent)
                 break;
 
             case ClientMessage:
-                if (static_cast<Atom>(event.xclient.data.l[0]) == m_wmDeleteMessage)
+                if (std::cmp_equal(event.xclient.data.l[0], m_wmDeleteMessage))
                 {
                     m_shouldClose = true;
                     e.type = WindowEventType::Close;
