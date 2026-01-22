933937f1ca57afa1f2fd2d84e4228cd704e234fc
MASINA Elliot <elliot.masina@epitech.eu>
2026-01-22 23:03:43 +0100

    Build: improve cmake usage

--- cmake/modules/ClangFormat.cmake
+++ cmake/modules/ClangFormat.cmake
@@ -1,22 +1,16 @@
-option(CAE_CLANG_FORMAT "Enable clang-format" OFF)
+function(format_project FILES_TO_FORMAT)
 
-if (NOT CAE_CLANG_FORMAT)
-    return()
-endif()
+    find_program(CLANG_FORMAT_EXE NAMES clang-format REQUIRED)
 
-if(TARGET ${PROJECT_NAME}-clang-format OR TARGET ${PROJECT_NAME}-clang-format-check)
-    return()
-endif()
+    add_custom_target(${PROJECT_NAME}-clang-format
+            COMMAND ${CLANG_FORMAT_EXE} -style=file -i ${FILES_TO_FORMAT}
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            COMMENT "Formatting sources with clang-format"
+    )
+    add_custom_target(${PROJECT_NAME}-clang-format-check
+            COMMAND ${CLANG_FORMAT_EXE} --dry-run --Werror -style=file ${FILES_TO_FORMAT}
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            COMMENT "Checking source formatting"
+    )
 
-find_program(CLANG_FORMAT_EXE NAMES clang-format REQUIRED)
-
-add_custom_target(${PROJECT_NAME}-clang-format
-        COMMAND ${CLANG_FORMAT_EXE} -style=file -i ${FILES_TO_FORMAT}
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        COMMENT "Formatting sources with clang-format"
-)
-add_custom_target(${PROJECT_NAME}-clang-format-check
-        COMMAND ${CLANG_FORMAT_EXE} --dry-run --Werror -style=file ${FILES_TO_FORMAT}
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        COMMENT "Checking source formatting"
-)
+endfunction()
