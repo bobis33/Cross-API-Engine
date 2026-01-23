630e17b89585766dd02fe8db0c3ecd1609aefa2e
MASINA Elliot <elliot.masina@epitech.eu>
2026-01-23 12:27:44 +0100

    Build: improve documentatyion generation usage
    export 2 functions; one that generate pdf, second generate html. Use cmake option to tell cmake wich one to generate

--- cmake/modules/MakeDoc.cmake
+++ cmake/modules/MakeDoc.cmake
@@ -1,29 +1,37 @@
-option(CAE_BUILD_DOC "Build documentation" OFF)
-
-if (NOT CAE_BUILD_DOC)
-    return()
-endif()
+include_guard(GLOBAL)
 
 find_package(Doxygen REQUIRED)
 
-file(DOWNLOAD https://raw.githubusercontent.com/jothepro/doxygen-awesome-css/v2.4.1/doxygen-awesome.css
-        ${CMAKE_CURRENT_BINARY_DIR}/doxygen-awesome.css)
-file(DOWNLOAD https://raw.githubusercontent.com/jothepro/doxygen-awesome-css/v2.4.1/doxygen-awesome-sidebar-only.css
-        ${CMAKE_CURRENT_BINARY_DIR}/doxygen-awesome-sidebar-only.css)
-
 set(DOXYGEN_DIR "${CMAKE_SOURCE_DIR}/documentation/.doxygen")
 set(DOXYFILE_OUT "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile")
-configure_file("${DOXYGEN_DIR}/Doxyfile" ${DOXYFILE_OUT} @ONLY)
-doxygen_add_docs(doxygen ${PROJECT_FILES})
-add_custom_command(TARGET doxygen POST_BUILD
-        WORKING_DIRECTORY ${DOXYGEN_DIR}
-        COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE_OUT}
-        VERBATIM
-)
-add_custom_command(TARGET doxygen POST_BUILD
-        WORKING_DIRECTORY "${DOXYGEN_DIR}/latex"
-        COMMAND make > /dev/null
-        COMMAND ${CMAKE_COMMAND} -E copy "refman.pdf" "${CMAKE_SOURCE_DIR}/documentation/${PROJECT_NAME}.pdf"
-        BYPRODUCTS "${CMAKE_SOURCE_DIR}/documentation/${PROJECT_NAME}.pdf"
-        VERBATIM
-)
+
+function(cae_download_extra_css)
+    file(DOWNLOAD "https://raw.githubusercontent.com/jothepro/doxygen-awesome-css/v2.4.1/doxygen-awesome.css"
+            "${DOXYGEN_DIR}/css/doxygen-awesome.css")
+    file(DOWNLOAD "https://raw.githubusercontent.com/jothepro/doxygen-awesome-css/v2.4.1/doxygen-awesome-sidebar-only.css"
+            "${DOXYGEN_DIR}/css/doxygen-awesome-sidebar-only.css")
+endfunction()
+
+function(cae_add_doxygen_html TARGET FILES)
+    configure_file("${DOXYGEN_DIR}/Doxyfile" ${DOXYFILE_OUT} @ONLY)
+    cae_download_extra_css()
+    doxygen_add_docs(${TARGET} ${FILES})
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+            WORKING_DIRECTORY ${DOXYGEN_DIR}
+            COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE_OUT}
+            VERBATIM
+    )
+endfunction()
+
+function(cae_add_doxygen_pdf TARGET FILES)
+    configure_file("${DOXYGEN_DIR}/Doxyfile" ${DOXYFILE_OUT} @ONLY)
+    cae_download_extra_css()
+    doxygen_add_docs(${TARGET} ${FILES})
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+            WORKING_DIRECTORY "${DOXYGEN_DIR}/latex"
+            COMMAND make > /dev/null
+            COMMAND ${CMAKE_COMMAND} -E copy "refman.pdf" "${CMAKE_SOURCE_DIR}/documentation/${PROJECT_NAME}.pdf"
+            BYPRODUCTS "${CMAKE_SOURCE_DIR}/documentation/${PROJECT_NAME}.pdf"
+            VERBATIM
+    )
+endfunction()
